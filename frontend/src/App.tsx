0c1334f126ea82d5d85c55cde2321b41b1f5938f
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2025-08-28 22:33:50 +0900

    chore: axios.ts と App.tsx を更新

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,5 @@
-import { useEffect } from 'react'
 import AppRoutes from './routes'
-import { bootstrapCsrf } from './lib/axios'
 
 export const App = () => {
-  useEffect(() => {
-    bootstrapCsrf().catch(console.error)
-  }, [])
   return <AppRoutes />
 }
